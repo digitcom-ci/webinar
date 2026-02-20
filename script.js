64d93d816fa7cea97d70b2798ab39f4fc62af9cf
Doumbia <dd.somanager@gmail.com>
2026-02-20 15:44:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const form = document.getElementById("formulaire");
+const acces = document.getElementById("acces");
 
+// déjà inscrit
+if(localStorage.getItem("afg_webinar")){
+    form.style.display="none";
+    acces.style.display="block";
+}
+
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+
+    const data={
+        nom:nom.value,
+        email:email.value,
+        tel:tel.value,
+        date:new Date()
+    };
+
+    localStorage.setItem("afg_webinar",JSON.stringify(data));
+
+    form.style.display="none";
+    acces.style.display="block";
+});
